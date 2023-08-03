b3b932a19efc9e52f3b81219559d9d460b2f5765
sanikani <sanikani@naver.com>
2023-08-04 01:03:46 +0900

    2023-08-04 study

---
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,8 @@
+package hello.servlet.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "memberSaveServlet",urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet {
+
+}
