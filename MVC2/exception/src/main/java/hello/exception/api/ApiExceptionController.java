cec50b63b48b80797e5f1058fad8d461f5d53d55
sanikani <sanikani@naver.com>
2023-09-21 16:31:25 +0900

    ExceptionResolver 적용2

--- MVC2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ MVC2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -5,9 +5,12 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -31,6 +34,16 @@ public class ApiExceptionController {
     @GetMapping("/api/response-status-ex1")
     public String responseStatusEx1() {
         throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad",new IllegalStateException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
