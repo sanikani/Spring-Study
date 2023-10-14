648422163bdcf014fd2633a6c49c6edd03d7cf71
sanikani <sanikani@naver.com>
2023-10-14 22:50:09 +0900

    데이터 접근 기술 - JPA

--- DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -16,7 +16,7 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 @Slf4j
 public class ItemServiceApplication {
