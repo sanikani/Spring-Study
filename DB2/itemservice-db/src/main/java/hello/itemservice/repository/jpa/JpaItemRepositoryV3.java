b68dcd5812a1d608f392b713a713636f045a803b
sanikani <sanikani@naver.com>
2023-10-19 00:18:27 +0900

    데이터 접근 기술 - 활용 방안

--- DB2/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ DB2/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,6 +1,8 @@
 package hello.itemservice.repository.jpa;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
 import hello.itemservice.domain.QItem;
@@ -49,8 +51,7 @@ public class JpaItemRepositoryV3 implements ItemRepository {
         return Optional.ofNullable(item);
     }
 
-    @Override
-    public List<Item> findAll(ItemSearchCond cond) {
+    public List<Item> findAllOld(ItemSearchCond cond) {
         String itemName = cond.getItemName();
         Integer maxPrice = cond.getMaxPrice();
         BooleanBuilder builder = new BooleanBuilder();
@@ -68,5 +69,31 @@ public class JpaItemRepositoryV3 implements ItemRepository {
                 .fetch();
 
         return result;
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        return query
+                .select(item)
+                .from(item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
     }
 }
