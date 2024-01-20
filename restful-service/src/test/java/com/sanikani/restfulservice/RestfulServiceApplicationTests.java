631514404a6125371fa5267912dfbc395c43952f
sanikani <sanikani@naver.com>
2024-01-20 18:31:59 +0900

    initial commit

---
+++ restful-service/src/test/java/com/sanikani/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanikani.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
