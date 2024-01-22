6aeb1a8e1ec97d96d13513f988003c9b76223051
sanikani <sanikani@naver.com>
2024-01-22 23:33:04 +0900

    User domain,User dao 구현

---
+++ restful-service/src/main/java/com/sanikani/restfulservice/dao/UserDaoService.java
@@ -0,0 +1,49 @@
+package com.sanikani.restfulservice.dao;
+
+import com.sanikani.restfulservice.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    static {
+        users.add(new User(1, "aaa", new Date()));
+        users.add(new User(2, "bbb", new Date()));
+        users.add(new User(3, "ccc", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCount);
+        }
+
+        if (user.getJoinDate() == null) {
+            user.setJoinDate(new Date());
+        }
+
+        users.add(user);
+
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
