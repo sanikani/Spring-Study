4c965194326c2d916ddc3c63c11e6016453b7aa6
sanikani <sanikani@naver.com>
2024-01-28 02:34:38 +0900

    의존성 추가, localeResolver bean 등록

--- restful-service/src/main/java/com/sanikani/restfulservice/RestfulServiceApplication.java
+++ restful-service/src/main/java/com/sanikani/restfulservice/RestfulServiceApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 }
